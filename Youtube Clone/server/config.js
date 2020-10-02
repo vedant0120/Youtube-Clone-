a734a11b108156a3221e55f6f7b31a98cc30a1aa
vedant0120 <61488624+vedant0120@users.noreply.github.com>
2020-10-02 16:04:01 -0400

    Update config.js

--- Youtube Clone/server/config.js
+++ Youtube Clone/server/config.js
@@ -7,6 +7,5 @@ module.exports = {
   MONGODB_URL: process.env.MONGODB_URL,
   SALT_ROUNDS: process.env.SALT_ROUNDS,
   JWT_SECRET: process.env.JWT_SECRET,
-  //   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
-  //   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
+
 };
